17f45f170e7e4ee22772af67ea17bc153d10f502
farukade <farukadekunle@gmail.com>
2024-08-06 21:43:10 +0100

    rebuild script updated

--- rebuild.sh
+++ rebuild.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Navigate to your project directory
-cd /home/techtink/erp || exit
+cd /root/nerve.io || exit
 
 # Pull the latest changes from the GitHub repository
 git pull origin main
 
 # Stop service
-pm2 stop erp
+pm2 stop nerve.io
 
 # Rebuild the Node.js application
 yarn install
 yarn build
 
 # Restart the PM2 instance
-pm2 restart erp
-pm2 logs erp
+pm2 restart nerve.io
+pm2 logs nerve.io
 
